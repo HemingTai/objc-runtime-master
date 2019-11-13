ff6e4da931d4df6b805abacb5b3d7685ff477b12
Hem1ngTai <guojin_jsn@Hem1ngTais-MacBook-Pro.local>
2019-11-13 17:07:10 +0800

    update

--- Debug-Objc-App/main.m
+++ Debug-Objc-App/main.m
@@ -8,7 +8,27 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+int global_i = 1;
+static int static_global_j = 2;
+
 int main(int argc, char * argv[]) {
+    static int static_k = 3;
+    int val = 4;
+    
+    void (^block)(void) = ^{
+        global_i ++;
+        static_global_j ++;
+        static_k ++;
+        NSLog(@"i:%d,j:%d,k:%d,v:%d",global_i,static_global_j,static_k,val);
+    };
+    global_i ++;
+    static_global_j ++;
+    static_k ++;
+    val ++;
+    NSLog(@"---i:%d,j:%d,k:%d,v:%d",global_i,static_global_j,static_k,val);
+    
+    block();
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
